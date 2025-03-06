ac9ae2a9cdd3b8d22ac3537786f592a75fbda03c
Colin Hughes <colin@w4b.co.uk>
2025-03-06 12:04:46 +0000

    Update configuration and enhance chat functionality
    - Added endpoint to the config.
    - Included a new package for prompt templates.
    - Adjusted file path for app settings.
    - Implemented Azure OpenAI chat completion in the program.
    - Created methods for managing chat history and user input.
    - Added prompts for travel recommendations and language tips.

--- M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
+++ M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
@@ -1,10 +1,9 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.ChatCompletion;
-using Microsoft.SemanticKernel.Connectors.OpenAI;
 
-string filePath = Path.GetFullPath("../../appsettings.json");
-var config = new ConfigurationBuilder()
+string filePath = Path.GetFullPath("../../../../../appsettings.json");
+IConfigurationRoot config = new ConfigurationBuilder()
     .AddJsonFile(filePath)
     .Build();
 
@@ -13,6 +12,61 @@ string modelId = config["modelId"]!;
 string endpoint = config["endpoint"]!;
 string apiKey = config["apiKey"]!;
 
-//
-// Add your code
-//
+// Create a kernel with Azure OpenAI chat completion
+IKernelBuilder builder = Kernel.CreateBuilder();
+builder.AddAzureOpenAIChatCompletion(modelId, endpoint, apiKey);
+
+// Build the kernel
+Kernel kernel = builder.Build();
+
+// Get chat completion service.
+IChatCompletionService chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Create a chat history object
+ChatHistory chatHistory = [];
+
+void AddMessage(string msg) {
+    Console.WriteLine(msg);
+    chatHistory.AddAssistantMessage(msg);
+}
+
+void GetInput() {
+    string input = Console.ReadLine()!;
+    chatHistory.AddUserMessage(input);
+}
+
+async Task GetReply() {
+    ChatMessageContent reply = await chatCompletionService.GetChatMessageContentAsync(
+        chatHistory,
+        kernel: kernel
+    );
+    Console.WriteLine(reply.ToString());
+    chatHistory.AddAssistantMessage(reply.ToString());
+}
+
+// Prompt the LLM
+chatHistory.AddSystemMessage("You are a helpful travel assistant.");
+chatHistory.AddSystemMessage("Recommend a destination to the traveler based on their background and preferences.");
+
+// Get information about the user's plans
+AddMessage("Tell me about your travel plans.");
+GetInput();
+await GetReply();
+
+// Offer recommendations
+AddMessage("Would you like some activity recommendations?");
+GetInput();
+await GetReply();
+
+// Offer language tips
+AddMessage("Would you like some helpful phrases in the local language?");
+GetInput();
+await GetReply();
+
+Console.WriteLine("Chat Ended.\n");
+Console.WriteLine("Chat History:");
+
+for (int i = 0; i < chatHistory.Count; i++) {
+    Console.WriteLine($"{chatHistory[i].Role}: {chatHistory[i]}");
+}
+Console.ReadLine();
